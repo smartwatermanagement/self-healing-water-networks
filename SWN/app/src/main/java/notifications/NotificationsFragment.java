7632916066169cf2b6918a24dfda677a21e9f759
kumudinikakwani <kumudinikakwani13@gmail.com>
2014-09-30 14:28:40 +0530

    Notification Details changed

--- SWN/app/src/main/java/notifications/NotificationsFragment.java
+++ SWN/app/src/main/java/notifications/NotificationsFragment.java
@@ -334,7 +334,7 @@ public class NotificationsFragment extends Fragment {
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             // Use the Builder class for convenient dialog construction
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setMessage(notificationDetails)
+            builder.setMessage("Assigned To: A.S. Rudraswamy" + notificationDetails)
                     .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
                             dialog.dismiss();
