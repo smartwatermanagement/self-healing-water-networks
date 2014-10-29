704c290330b394060b6ba20b21d32ae733e647e7
AbhijithMadhav <browseanddownload@gmail.com>
2014-10-29 19:18:36 +0530

    Modified DB design

--- SWNBackend/DB SQL FIle/swndb.sql
+++ SWNBackend/DB SQL FIle/swndb.sql
@@ -6,7 +6,13 @@ create table connections(from_id int, to_id int, property varchar(255), value te
  
 create table thresholds (id int, asset_id int, property varchar(255) not null, operator varchar(255) not null, value text not null, primary key (id), foreign key (asset_id) references assets(id));
  
-create table issues(id int, asset_id int, aggregation_id int, type enum('level', 'quality', 'leak', 'shortage', 'water-requirement') not null, status enum('new', 'in-progress', 'resolved') not null default 'new', details text, primary key (id), foreign key (asset_id) references assets(id), foreign key (aggregation_id) references aggregations(id));
+create table issues(id int, asset_id int, aggregation_id int, type enum('thresholdbreach', 'leak', 'water-requirement-prediction', 'water-garden') not null, status enum('new', 'in-progress', 'resolved') not null default 'new', details text, primary key (id), foreign key (asset_id) references assets(id), foreign key (aggregation_id) references aggregations(id));
 
-create table user(id int, name text not null, phone_number varchar(255), username varchar(255) not null unique, password varchar(255) not null, primary key (id));
+create table users(id int, name text not null, phone_number varchar(255), username varchar(255) not null unique, password varchar(255) not null, primary key (id));
+
+create table subscriptions(id int, issueType enum('thresholdbreach', 'leak', 'water-requirement-prediction', 'water-garden') not null, user_id int, aggregation_id int, primary key (id), foreign key (user_id) references users(id), foreign key (aggregation_id) references aggregations(id)); 
+
+create table notifications(id int, read_status boolean default false, primary key (id), user_id int, issue_id int, foreign key (user_id) references users(id), foreign key (issue_id) references issues(id));
+
+
  
