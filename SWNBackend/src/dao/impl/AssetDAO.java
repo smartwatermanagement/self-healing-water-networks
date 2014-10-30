8f5828b8ed25e84b685e6f88677d2d8713b281a8
kumudinikakwani <kumudinikakwani13@gmail.com>
2014-10-30 10:58:38 +0530

    AssetDAO.

--- SWNBackend/src/dao/impl/AssetDAO.java
+++ SWNBackend/src/dao/impl/AssetDAO.java
@@ -3,6 +3,7 @@ package dao.impl;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,9 +18,10 @@ public class AssetDAO implements IAssetDAO{
 
 	private static final String findByIdQuery = "SELECT * FROM assets WHERE id=?";
 	private static final String findByAggregationQuery = "SELECT * FROM assets WHERE aggregation_id=?";
+	private static final String findAllQuery = "SELECT * FROM assets";
 
 	public Asset findById(int id) {
-		
+
 		Asset asset = null;
 		Map<String, String> propertyValues = new HashMap<String, String>();
 		Connection connection = null;
@@ -38,13 +40,13 @@ public class AssetDAO implements IAssetDAO{
 				asset = new Asset(id, null, issueCount, type, latitude, longitude,null);
 				propertyValues.put(resultSet.getString("property"), resultSet.getString("values"));
 			}
-			
+
 			while(resultSet.next()){
 				propertyValues.put(resultSet.getString("property"), resultSet.getString("values"));
 			}
 			asset.setPropertyValueMap(propertyValues);
 			asset.setThresholds((new ThresholdDAO()).findByAsset(asset));
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally{
@@ -54,13 +56,84 @@ public class AssetDAO implements IAssetDAO{
 	}
 
 	public List<Asset> findAssestsByAggregation(Aggregation aggregation) {
-		// TODO Auto-generated method stub
-		return null;
+
+		Map<Integer, Asset> assetsMap = new HashMap<Integer, Asset>();
+		Connection connection = null;
+		PreparedStatement statement = null;
+
+		try {
+			connection = ConnectionPool.getConnection();
+			statement = connection.prepareStatement(findByAggregationQuery);
+			statement.setInt(1, aggregation.getId());
+			ResultSet resultSet = statement.executeQuery();
+			while(resultSet.next()){
+				if(!assetsMap.containsKey(resultSet.getInt("id"))){
+					Asset asset;
+					Map<String, String> propertyValues = new HashMap<String, String>();
+					int currentId = resultSet.getInt("id");
+					AssetType type = AssetType.valueOf(resultSet.getString("type"));
+					int issueCount = resultSet.getInt("issue_count");
+					double latitude = resultSet.getDouble("latitude");
+					double longitude = resultSet.getDouble("longitude");
+					asset = new Asset(currentId, null, issueCount, type, latitude, longitude,null);
+					propertyValues.put(resultSet.getString("property"), resultSet.getString("values"));
+					asset.setPropertyValueMap(propertyValues);
+					asset.setThresholds((new ThresholdDAO()).findByAsset(asset));
+					assetsMap.put(currentId, asset);
+				}
+				else{
+					assetsMap.get(resultSet.getInt("id")).getPropertyValueMap()
+					.put(resultSet.getString("property"), resultSet.getString("values"));
+				}
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally{
+			ConnectionPool.freeConnection(connection);
+		}
+		return new ArrayList<Asset>(assetsMap.values());
 	}
 
 	public List<Asset> findAllAssets() {
-		// TODO Auto-generated method stub
-		return null;
+
+		Map<Integer, Asset> assetsMap = new HashMap<Integer, Asset>();
+		Connection connection = null;
+		PreparedStatement statement = null;
+
+		try {
+			connection = ConnectionPool.getConnection();
+			statement = connection.prepareStatement(findAllQuery);
+			ResultSet resultSet = statement.executeQuery();
+			while(resultSet.next()){
+				if(!assetsMap.containsKey(resultSet.getInt("id"))){
+					Asset asset;
+					Map<String, String> propertyValues = new HashMap<String, String>();
+					int currentId = resultSet.getInt("id");
+					AssetType type = AssetType.valueOf(resultSet.getString("type"));
+					int issueCount = resultSet.getInt("issue_count");
+					double latitude = resultSet.getDouble("latitude");
+					double longitude = resultSet.getDouble("longitude");
+					asset = new Asset(currentId, null, issueCount, type, latitude, longitude,null);
+					propertyValues.put(resultSet.getString("property"), resultSet.getString("values"));
+					asset.setPropertyValueMap(propertyValues);
+					asset.setThresholds((new ThresholdDAO()).findByAsset(asset));
+					assetsMap.put(currentId, asset);
+				}
+				else{
+					assetsMap.get(resultSet.getInt("id")).getPropertyValueMap()
+					.put(resultSet.getString("property"), resultSet.getString("values"));
+				}
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally{
+			ConnectionPool.freeConnection(connection);
+		}
+		return new ArrayList<Asset>(assetsMap.values());
 	}
 
 }
