b7bb49681dc0fc42dddacf2d854776644c5dd173
kumudinikakwani <kumudinikakwani13@gmail.com>
2014-10-30 11:00:16 +0530

    AssetDAO.

--- SWNBackend/src/model/Aggregation.java
+++ SWNBackend/src/model/Aggregation.java
@@ -41,29 +41,6 @@ public class Aggregation {
 		this.assets = assets;
 	}
 	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		return result;
-	}
-
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Asset other = (Asset) obj;
-		if (id != other.id)
-			return false;
-		return true;
-	}
-	
 	
 
 }
