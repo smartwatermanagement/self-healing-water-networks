8c41ecf58382341d8012e29daa66e523747159c0
AbhijithMadhav <browseanddownload@gmail.com>
2014-11-01 16:57:12 +0530

    Files missed out in the last commit

---
+++ SWNBackend/src/action/nonrest/WaterUsageAction.java
@@ -0,0 +1,8 @@
+package action.nonrest;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class WaterUsageAction extends ActionSupport
+{
+
+}
---
+++ SWNBackend/src/model/WaterUsageByAggregation.java
@@ -0,0 +1,23 @@
+package model;
+
+public class WaterUsageByAggregation
+{
+	private String name;
+	private double usage; // in litres
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public double getUsage()
+	{
+		return usage;
+	}
+	public void setUsage(double usage)
+	{
+		this.usage = usage;
+	}
+}
