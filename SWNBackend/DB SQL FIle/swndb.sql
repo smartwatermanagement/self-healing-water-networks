3085dd4589ea87b0c8ab1cf36504175d88fa7ed7
AbhijithMadhav <browseanddownload@gmail.com>
2014-11-03 00:38:08 +0530

    Network DB design changes

--- SWNBackend/DB SQL FIle/swndb.sql
+++ SWNBackend/DB SQL FIle/swndb.sql
@@ -1,12 +1,14 @@
-create table aggregations ( id int auto_increment, aggregation_name text, issue_count int default 0, parent_id int, primary key (id) , foreign key (parent_id) references aggregations(id));
+create table aggregations (id int auto_increment, aggregation_name text not null, issue_count int default 0, parent_id int, primary key (id) , foreign key (parent_id) references aggregations(id));
 
-create table assets (id int, type enum('storage', 'source', 'outlet', 'pump', 'recycling_plant'), issue_count int default 0, latitude double, longitude double, primary key(id), parent_aggregation_id int, foreign key (parent_aggregation_id) references aggregations(id));
+create table assets (id int auto_increment, type enum('storage', 'source', 'outlet', 'pump', 'recycling_plant', 'connection') not null, issue_count int default 0, latitude double not null, longitude double not null, primary key(id), parent_aggregation_id int, foreign key (parent_aggregation_id) references aggregations(id));
 
-create table asset_property_value_map(id int, property varchar(255) not null, value text not null, asset_id int, primary key (id), foreign key(asset_id) references asset);
+create table asset_property_value_map(id int auto_increment, property varchar(255) not null, value text not null, asset_id int not null, primary key (id), foreign key(asset_id) references assets(id));
 
-create table connections(from_id int, to_id int, property varchar(255) not null, value text not null, foreign key (from_id) references assets(id), foreign key (to_id) references assets(id));
+create table connections(id int auto_increment, from_id int not null, to_id int not null, primary key(id), foreign key (from_id) references assets(id), foreign key (to_id) references assets(id));
 
-create table thresholds (id int auto_increment, asset_id int, property varchar(255) not null, operator varchar(255) not null, value text not null, primary key (id), foreign key (asset_id) references assets(id));
+create table sensors(id int, asset_id int not null, primary key(id), foreign key(asset_id) references assets(id));
+
+create table thresholds (id int auto_increment, asset_id int not null, property varchar(255) not null, operator varchar(255) not null, value text not null, primary key (id), foreign key (asset_id) references assets(id));
  
 create table issues(id int auto_increment , asset_id int, aggregation_id int, type enum('threshold_breach', 'leak', 'water_requirement_prediction', 'water_garden') not null, status enum('new', 'in_progress', 'resolved') not null default 'new', details text, created_at timestamp, updated_at timestamp, primary key (id), foreign key (asset_id) references assets(id), foreign key (aggregation_id) references aggregations(id));
 
